99dcbcb14a4660717114f15316c4bafa81516a19
Byeongsoon Jang <byeongsoon94@gmail.com>
2021-08-15 22:54:36 +0900

    Add JpaConfig

---
+++ src/main/java/demo/consumerservice/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package demo.consumerservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}

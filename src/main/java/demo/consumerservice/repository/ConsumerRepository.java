77910f806a33edfce1b42f3d1d12f41257df5dca
Byeongsoon Jang <byeongsoon94@gmail.com>
2021-08-16 13:29:51 +0900

    issue#2 Repository 생성

---
+++ src/main/java/demo/consumerservice/repository/ConsumerRepository.java
@@ -0,0 +1,9 @@
+package demo.consumerservice.repository;
+
+import demo.consumerservice.model.entity.Consumer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
+}
